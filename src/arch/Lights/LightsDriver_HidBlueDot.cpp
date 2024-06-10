b3c6241da2ef71b69dab75a496dc4e7667099408
alessandro.dore <alessandro.dore@inpeco.com>
2024-06-10 21:41:10 +0200

    added first byte

--- src/arch/Lights/LightsDriver_HidBlueDot.cpp
+++ src/arch/Lights/LightsDriver_HidBlueDot.cpp
@@ -8,26 +8,26 @@ REGISTER_LIGHTS_DRIVER_CLASS(HidBlueDot);
 
 LightsDriver_HidBlueDot::LightsDriver_HidBlueDot()
 	:
-	m_Marquee_UpLeft(&m_iCabData[1], 0x01),
-	m_Marquee_UpRight(&m_iCabData[1], 0x02),
-	m_Marquee_LwLeft(&m_iCabData[1], 0x04),
-	m_Marquee_LwRight(&m_iCabData[1], 0x08),
-	m_Buttons_Left(&m_iCabData[1], 0x10),
-	m_Buttons_Right(&m_iCabData[1], 0x20),
-	m_Bass(&m_iCabData[1], 0x40),
-	m_Optional(&m_iCabData[1], 0x80),
+	m_Marquee_UpLeft(&m_iCabData[2], 0x01),
+	m_Marquee_UpRight(&m_iCabData[2], 0x02),
+	m_Marquee_LwLeft(&m_iCabData[2], 0x04),
+	m_Marquee_LwRight(&m_iCabData[2], 0x08),
+	m_Buttons_Left(&m_iCabData[2], 0x10),
+	m_Buttons_Right(&m_iCabData[2], 0x20),
+	m_Bass(&m_iCabData[2], 0x40),
+	m_Optional(&m_iCabData[2], 0x80),
 
-	m_PadP1_Up(&m_iPadData[1], 0x01),
-	m_PadP1_Down(&m_iPadData[1], 0x02),
-	m_PadP1_Left(&m_iPadData[1], 0x04),
-	m_PadP1_Right(&m_iPadData[1], 0x08),
-	m_PadP2_Up(&m_iPadData[1], 0x10),
-	m_PadP2_Down(&m_iPadData[1], 0x20),
-	m_PadP2_Left(&m_iPadData[1], 0x40),
-	m_PadP2_Right(&m_iPadData[1], 0x80)
+	m_PadP1_Up(&m_iPadData[2], 0x01),
+	m_PadP1_Down(&m_iPadData[2], 0x02),
+	m_PadP1_Left(&m_iPadData[2], 0x04),
+	m_PadP1_Right(&m_iPadData[2], 0x08),
+	m_PadP2_Up(&m_iPadData[2], 0x10),
+	m_PadP2_Down(&m_iPadData[2], 0x20),
+	m_PadP2_Left(&m_iPadData[2], 0x40),
+	m_PadP2_Right(&m_iPadData[2], 0x80)
 {
-	m_iCabData[0] = 0x01;
-	m_iPadData[0] = 0x02;
+	m_iCabData[1] = 0x01;
+	m_iPadData[1] = 0x02;
 	
 	// Open the device using the VID, PID,
 	// and optionally the Serial number.
@@ -62,7 +62,7 @@ void LightsDriver_HidBlueDot::Set(const LightsState *ls)
 	m_Buttons_Right.Set(ls->m_bGameButtonLights[PLAYER_2][GAME_BUTTON_START]);
 	m_Bass.Set(ls->m_bCabinetLights[LIGHT_BASS_LEFT] || ls->m_bCabinetLights[LIGHT_BASS_RIGHT]);
 	
-	hid_write(handle, (unsigned char *)&m_iCabData, 2);
+	hid_write(handle, (unsigned char *)&m_iCabData, 3);
 
 	//! Set pad lights.
 	m_PadP1_Left.Set(ls->m_bGameButtonLights[PLAYER_1][DANCE_BUTTON_LEFT]);
@@ -74,5 +74,5 @@ void LightsDriver_HidBlueDot::Set(const LightsState *ls)
 	m_PadP2_Up.Set(ls->m_bGameButtonLights[PLAYER_2][DANCE_BUTTON_UP]);
 	m_PadP2_Down.Set(ls->m_bGameButtonLights[PLAYER_2][DANCE_BUTTON_DOWN]);
 
-	hid_write(handle, (unsigned char *)&m_iPadData, 2);
+	hid_write(handle, (unsigned char *)&m_iPadData, 3);
 }
